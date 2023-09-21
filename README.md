53d93e270f2a38172d4cb13a378a936795f5a0c1
RiverNA <longqiwf@outlook.com>
2023-09-22 00:15:25 +0800

    submit

--- README.md
+++ README.md
@@ -10,9 +10,11 @@ Fei Wu, Nora Gourmelon, Thorsten Seehaus, Jianlin Zhang, Matthias Braun, Andreas
 ### Abstract
 Knowledge on changes in glacier calving front positions is important for assessing the status of glaciers. Remote sensing imagery provides the ideal database for monitoring calving front positions, however, it is not feasible to perform this task manually for all calving glaciers globally due to time-constraints. Deep learning-based methods have shown great potential for glacier calving front delineation from optical and radar satellite imagery. The calving front is represented as a single thin line between the ocean and the glacier, which makes the task vulnerable to inaccurate predictions. The limited availability of annotated glacier imagery leads to a lack of data diversity (not all possible combinations of different weather conditions, terminus shapes, sensors, etc. are present in the data), which exacerbates the difficulty of accurate segmentation. In this paper, we propose Attention-Multi-hooking-Deep-supervision HookNet (AMD-HookNet), a novel glacier calving front segmentation framework for synthetic aperture radar (SAR) images. The proposed method aims to enhance the feature representation capability through multiple information interactions between low-resolution and high-resolution inputs based on a two-branch U-Net. The attention mechanism, integrated into the two branch U-Net, aims to interact between the corresponding coarse and fine-grained feature maps. This allows the network to automatically adjust feature relationships, resulting in accurate pixel-classification predictions. Extensive experiments and comparisons on the challenging glacier segmentation benchmark dataset CaFFe show that our AMD-HookNet achieves a mean distance error of 438m to the ground truth outperforming the current state of the art by 42%, which validates its effectiveness.
 
-### Inference
-Please refer to [CaFFe](https://github.com/Nora-Go/Calving_Fronts_and_Where_to_Find_Them)
+### Preprocessing
+Prepare train/valid/test datasets based on [CaFFe](https://github.com/Nora-Go/Calving_Fronts_and_Where_to_Find_Them), run center_crop.py, Sliding_window_generate_dataset.py, generate_target_context.py accordingly.
 
+### Inference
+Please refer to [CaFFe](https://github.com/Nora-Go/Calving_Fronts_and_Where_to_Find_Them) which offers a uniform standard postprocessing and analyzing tool for evaluating experimental results.
 ### License
 Licensed under an MIT license.
 
---
+++ Upload/generate_target_context.py
@@ -0,0 +1,179 @@
+import glob
+import os
+import PIL
+import numpy as np3
+from config import cfg
+from PIL import Image
+from torchvision import transforms
+from torchvision.utils import save_image
+
+ti = './Sliding_window/Train/Transform_images'
+mi = './Sliding_window/Train/Transform_masks'
+
+images = sorted(glob.glob(os.path.join(ti, '*.png')))
+masks = sorted(glob.glob(os.path.join(mi, '*.png')))
+
+save_path_target = './Sliding_window/Train/Training/target_images'
+save_path_context = './Sliding_window/Train/Training/context_images'
+save_path_target_mask = './Sliding_window/Train/Training/target_masks'
+save_path_context_mask = './Sliding_window/Train/Training/context_masks'
+
+size = 288
+
+target = transforms.Compose([
+    transforms.CenterCrop((size, size)),
+    transforms.ToTensor(),
+])
+
+context_images = transforms.Compose([
+    transforms.CenterCrop((size * 2, size * 2)),
+    transforms.Resize((size, size), interpolation=PIL.Image.BICUBIC),
+    transforms.ToTensor(),
+])
+
+context_masks = transforms.Compose([
+    transforms.CenterCrop((size * 2, size * 2)),
+    transforms.Resize((size, size), interpolation=PIL.Image.NEAREST),
+    transforms.ToTensor(),
+])
+
+if not os.path.exists(save_path_target):
+    os.makedirs(save_path_target)
+if not os.path.exists(save_path_context):
+    os.makedirs(save_path_context)
+if not os.path.exists(save_path_target_mask):
+    os.makedirs(save_path_target_mask)
+if not os.path.exists(save_path_context_mask):
+    os.makedirs(save_path_context_mask)
+
+for i in range(len(images)):
+    image = Image.open(images[i])
+    suffix = images[i].split('/')[-1].split('.')[0]
+
+    target_image = target(image)
+    context_image = context_images(image)
+    save_image(target_image, os.path.join(save_path_target, suffix + '.png'))
+    save_image(context_image, os.path.join(save_path_context, suffix + '.png'))
+
+for i in range(len(masks)):
+    mask = Image.open(masks[i])
+    suffix = masks[i].split('/')[-1].split('.')[0]
+
+    target_mask = target(mask)
+    context_mask = context_masks(mask)
+    save_image(target_mask, os.path.join(save_path_target_mask, suffix + '.png'))
+    save_image(context_mask, os.path.join(save_path_context_mask, suffix + '.png'))
+
+ti = './Sliding_window/Valid/Transform_images'
+mi = './Sliding_window/Valid/Transform_masks'
+
+images = sorted(glob.glob(os.path.join(ti, '*.png')))
+masks = sorted(glob.glob(os.path.join(mi, '*.png')))
+
+save_path_target = './Sliding_window/Valid/Validation/target_images'
+save_path_context = './Sliding_window/Valid/Validation/context_images'
+save_path_target_mask = './Sliding_window/Valid/Validation/target_masks'
+save_path_context_mask = './Sliding_window/Valid/Validation/context_masks'
+
+size = 288
+
+target = transforms.Compose([
+    transforms.CenterCrop((size, size)),
+    transforms.ToTensor(),
+])
+
+context_images = transforms.Compose([
+    transforms.CenterCrop((size * 2, size * 2)),
+    transforms.Resize((size, size), interpolation=PIL.Image.BICUBIC),
+    transforms.ToTensor(),
+])
+
+context_masks = transforms.Compose([
+    transforms.CenterCrop((size * 2, size * 2)),
+    transforms.Resize((size, size), interpolation=PIL.Image.NEAREST),
+    transforms.ToTensor(),
+])
+
+if not os.path.exists(save_path_target):
+    os.makedirs(save_path_target)
+if not os.path.exists(save_path_context):
+    os.makedirs(save_path_context)
+if not os.path.exists(save_path_target_mask):
+    os.makedirs(save_path_target_mask)
+if not os.path.exists(save_path_context_mask):
+    os.makedirs(save_path_context_mask)
+
+for i in range(len(images)):
+    image = Image.open(images[i])
+    suffix = images[i].split('/')[-1].split('.')[0]
+
+    target_image = target(image)
+    context_image = context_images(image)
+    save_image(target_image, os.path.join(save_path_target, suffix + '.png'))
+    save_image(context_image, os.path.join(save_path_context, suffix + '.png'))
+
+for i in range(len(masks)):
+    mask = Image.open(masks[i])
+    suffix = masks[i].split('/')[-1].split('.')[0]
+
+    target_mask = target(mask)
+    context_mask = context_masks(mask)
+    save_image(target_mask, os.path.join(save_path_target_mask, suffix + '.png'))
+    save_image(context_mask, os.path.join(save_path_context_mask, suffix + '.png'))
+
+ti = './Sliding_window/Test/Transform_images'
+mi = './Sliding_window/Test/Transform_masks'
+
+images = sorted(glob.glob(os.path.join(ti, '*.png')))
+masks = sorted(glob.glob(os.path.join(mi, '*.png')))
+
+save_path_target = './Sliding_window/Test/Testing/target_images'
+save_path_context = './Sliding_window/Test/Testing/context_images'
+save_path_target_mask = './Sliding_window/Test/Testing/target_masks'
+save_path_context_mask = './Sliding_window/Test/Testing/context_masks'
+
+size = 288
+
+target = transforms.Compose([
+    transforms.CenterCrop((size, size)),
+    transforms.ToTensor(),
+])
+
+context_images = transforms.Compose([
+    transforms.CenterCrop((size * 2, size * 2)),
+    transforms.Resize((size, size), interpolation=PIL.Image.BICUBIC),
+    transforms.ToTensor(),
+])
+
+context_masks = transforms.Compose([
+    transforms.CenterCrop((size * 2, size * 2)),
+    transforms.Resize((size, size), interpolation=PIL.Image.NEAREST),
+    transforms.ToTensor(),
+])
+
+if not os.path.exists(save_path_target):
+    os.makedirs(save_path_target)
+if not os.path.exists(save_path_context):
+    os.makedirs(save_path_context)
+if not os.path.exists(save_path_target_mask):
+    os.makedirs(save_path_target_mask)
+if not os.path.exists(save_path_context_mask):
+    os.makedirs(save_path_context_mask)
+
+for i in range(len(images)):
+    image = Image.open(images[i])
+    suffix = images[i].split('/')[-1].split('.')[0]
+
+    target_image = target(image)
+    context_image = context_images(image)
+    save_image(target_image, os.path.join(save_path_target, suffix + '.png'))
+    save_image(context_image, os.path.join(save_path_context, suffix + '.png'))
+
+for i in range(len(masks)):
+    mask = Image.open(masks[i])
+    suffix = masks[i].split('/')[-1].split('.')[0]
+
+    target_mask = target(mask)
+    context_mask = context_masks(mask)
+    save_image(target_mask, os.path.join(save_path_target_mask, suffix + '.png'))
+    save_image(context_mask, os.path.join(save_path_context_mask, suffix + '.png'))
